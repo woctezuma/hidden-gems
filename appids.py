c415481d200d6a81fb32f59186b93a6127d87d9a
Wok <woctezuma@users.noreply.github.com>
2017-11-29 00:16:55 +0100

    Set references of hidden gems in a separate file

---
+++ appids.py
@@ -0,0 +1,18 @@
+# Objective: list appID of examples of "hidden gems"
+
+# This is the appID of the game called "Contradiction".
+appidContradiction = "373390"
+
+# This is a set including appID of games which will serve as references of "hidden gems".
+# Reference: http://www.neogaf.com/forum/showpost.php?p=241232835&postcount=5886
+appid_hidden_gems_reference_set = {appidContradiction,
+                               "320090",
+                               "363980",
+                               "561740",
+                               "333300",
+                               "329970",
+                               "323220",
+                               "534290",
+                               "440880",
+                               "402040",
+                               "233980"}
