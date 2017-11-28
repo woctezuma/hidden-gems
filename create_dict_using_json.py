054b381a3327980babada9339a9eca97c285896a
Wok <woctezuma@users.noreply.github.com>
2017-11-28 17:35:06 +0100

    WIP simplify code for current steamSpy API

--- create_dict_using_json.py
+++ create_dict_using_json.py
@@ -1,38 +1,30 @@
 # Objective: store information regarding every Steam game in a dictionary.
+
+import urllib.request, json
+from download_json import downloadSteamSpyData
+from compute_wilson_score import computeWilsonScore
+
 #
 # Input:
 #
-# - a text file, manually copied from SteamDB, named "top_rated_games_on_steam.txt"
-input_filename = "top_rated_games_on_steam.txt"
-steamdb_url = "https://steamdb.info/stats/gameratings/?all"
-# NB: To download & copy the content of input_filename by yourself, you need to log in using your own Steam account on
-#     steamdb_url and then select to show all in the dropdown menu.
-#
-# - a json file, downloaded from SteamSpy, named "steamspy.json"
+# - SteamSpy's data in JSON format
 json_filename = "steamspy.json"
-steamspy_url = "http://steamspy.com/api.php?request=all"
-# NB: If json_filename is missing, the current script will attempt to download and cache it from steamspy_url.
+data = downloadSteamSpyData(json_filename)
 #
 # Output:
 #
 # a dictionary stored in a text file named "dict_top_rated_games_on_steam.txt"
 output_filename = "dict_top_rated_games_on_steam.txt"
 
-import urllib.request, json
-from compute_wilson_score import computeWilsonScore
-
 # Boolean to switch between hidden gems and hidden hidden gems (yes, twice hidden)
 use_hidden_squared_gems_as_examples = False
 max_num_reviews_for_hidden_squared_gems = 150
 
-# Optional boolean to decide whether we want to compute the Wilson score by ourselves, instead of using SteamDB's scores
-compute_our_own_wilson_score = True
 # This allows to specify a different confidence, which can turn out to be interesting.
 quantile_for_our_own_wilson_score = 0.95
 
 if use_hidden_squared_gems_as_examples:
     # Lower the confidence so that Wilson score is less punitive towards games with few reviews
-    compute_our_own_wilson_score = True
     quantile_for_our_own_wilson_score = 0.8
 
 # Booleans to decide whether we want to filter out and filter in games based on their Steam tags
@@ -55,11 +47,7 @@ else:
 appidContradiction = "373390"
 # This is a set including appID of games which will serve as references of "hidden gems", so we will make sure that
 # these games appear in the output dictionary, despite filter-out and filter-in.
-appid_default_reference_set = {appidContradiction}
-# A set of appID to use several games as references of hidden "hidden gems" (not a typo, twice hidden)
 # Reference: http://www.neogaf.com/forum/showpost.php?p=241232835&postcount=5886
-# appid_default_reference_set = {appidContradiction, "320090", "363980"}
-# A set of all the appID which people have suggested to me as examples of hidden gems, or hidden² gems
 appid_default_reference_set = {appidContradiction, "320090", "363980", "561740", "333300", "329970", "323220",
                                "534290", "440880", "402040", "233980"}
 
@@ -80,91 +68,54 @@ D = dict()
 
 verbose = False
 
-try:
-    with open(json_filename, 'r', encoding="utf8") as in_json_file:
-        data = json.load(in_json_file)
-except FileNotFoundError:
-    print("Downloading and caching data from SteamSpy")
-    # Trick to download the JSON file directly from SteamSpy, in case the file does not exist locally yet
-    # Reference: https://stackoverflow.com/a/31758803/
-    class AppURLopener(urllib.request.FancyURLopener):
-        version = "Mozilla/5.0"
-    opener = AppURLopener()
-    with opener.open(steamspy_url) as response:
-        data = json.load(response)
-        # Make sure the json data is using double quotes instead of single quotes
-        # Reference: https://stackoverflow.com/a/8710579/
-        jsonString = json.dumps(data)
-        # Cache the json data to a local file
-        with open(json_filename, 'w', encoding="utf8") as cache_json_file:
-            print(jsonString, file=cache_json_file)
+for appid in data.keys():
+    try:
+        name = data[appid]['name']
+        num_owners = data[appid]['owners']
+        num_players = data[appid]['players_forever']
+        median_time = data[appid]['median_forever']
+        average_time = data[appid]['average_forever']
+        num_positive_reviews = data[appid]["positive"]
+        num_negative_reviews = data[appid]["negative"]
+        tags_dict = data[appid]["tags"] #TODO remove as tags are not available anymore
 
-with open(input_filename, 'r', encoding="utf8") as infile:
-    for line in infile:
+        wilson_score = computeWilsonScore(num_positive_reviews, num_negative_reviews, quantile_for_our_own_wilson_score)
 
-        items = line.strip().split("\t")
-        stripped_items = list(map(str.strip, items))
+        stats_save = [name, wilson_score, num_owners, num_players, median_time, average_time, num_positive_reviews, num_negative_reviews]
 
-        appid = stripped_items[0]
-        name = stripped_items[1]
-
-        toInteger = lambda str : int(str.replace(',',''))
-
-        num_positive_reviews = toInteger(stripped_items[2])
-        num_negative_reviews = toInteger(stripped_items[3])
-
-        toPercentage = lambda str : float(str.strip("%"))/100
-
-        arbitrary_score_from_SteamDB = toPercentage(stripped_items[-1])
-
-        if compute_our_own_wilson_score:
-            wilson_score = computeWilsonScore(num_positive_reviews, num_negative_reviews, quantile_for_our_own_wilson_score)
+        if len(tags_dict) == 0:
+            tags = set()
         else:
-            # This is not the Wilson score anymore. See this blog post: https://steamdb.info/blog/steamdb-rating/
-            wilson_score = arbitrary_score_from_SteamDB
+            tags = set(tags_dict.keys())
 
-        try:
-            num_owners = data[appid]['owners']
-            num_players = data[appid]['players_forever']
-            median_time = data[appid]['median_forever']
-            average_time = data[appid]['average_forever']
-            tags_dict = data[appid]["tags"]
+        # Check filter-out
+        boolPassFilterOut = bool(len(tags_to_filter_out.intersection(tags)) == 0)
+        # Check filter-in: either there is no filter-in, or there is filter-in and at least one of the desired tags
+        boolPassFilterIn = bool(len(tags_to_filter_in) == 0) or bool(len(tags_to_filter_in.intersection(tags)) != 0)
+        boolGameShouldAppearInRanking = (boolPassFilterOut and boolPassFilterIn)
+        # We save the boolean due to the presence of a reference game, which we have to include in the dictionary,
+        # but which may not appear in the final ranking due to the tag filters.
+        stats_save.append(boolGameShouldAppearInRanking)
 
-            stats_save = [name, wilson_score, num_owners, num_players, median_time, average_time, num_positive_reviews, num_negative_reviews]
+        # Make sure the output dictionary includes the game which will be chosen as a reference of a "hidden gem"
+        if appid in appid_default_reference_set:
+            # If we look for hidden squared gems, then we don't want to use examples with many reviews,
+            # because these games would be examples of hidden gems, but not examples of "hidden hidden" gems.
+            num_reviews = num_positive_reviews + num_negative_reviews
+            if not(use_hidden_squared_gems_as_examples) or (num_reviews <= max_num_reviews_for_hidden_squared_gems):
+                print("Game used as a reference:\t" + name + "\t(appID=" + appid + ")")
+                # If a game is the reference game, we have include it in the dictionary
+                D[appid] = stats_save
 
-            if len(tags_dict) == 0:
-                tags = set()
-            else:
-                tags = set(tags_dict.keys())
+        else:
+            # If a game is not the reference game, then it may only appear in the ranking if it passes the tag filters.
+            if boolGameShouldAppearInRanking:
+                D[appid] = stats_save
 
-            # Check filter-out
-            boolPassFilterOut = bool(len(tags_to_filter_out.intersection(tags)) == 0)
-            # Check filter-in: either there is no filter-in, or there is filter-in and at least one of the desired tags
-            boolPassFilterIn = bool(len(tags_to_filter_in) == 0) or bool(len(tags_to_filter_in.intersection(tags)) != 0)
-            boolGameShouldAppearInRanking = (boolPassFilterOut and boolPassFilterIn)
-            # We save the boolean due to the presence of a reference game, which we have to include in the dictionary,
-            # but which may not appear in the final ranking due to the tag filters.
-            stats_save.append(boolGameShouldAppearInRanking)
-
-            # Make sure the output dictionary includes the game which will be chosen as a reference of a "hidden gem"
-            if appid in appid_default_reference_set:
-                # If we look for hidden squared gems, then we don't want to use examples with many reviews,
-                # because these games would be examples of hidden gems, but not examples of "hidden hidden" gems.
-                num_reviews = num_positive_reviews + num_negative_reviews
-                if not(use_hidden_squared_gems_as_examples) or (num_reviews <= max_num_reviews_for_hidden_squared_gems):
-                    print("Game used as a reference:\t" + name + "\t(appID=" + appid + ")")
-                    # If a game is the reference game, we have include it in the dictionary
-                    D[appid] = stats_save
-
-            else:
-                # If a game is not the reference game, then it may only appear in the ranking if it passes the tag filters.
-                if boolGameShouldAppearInRanking:
-                    D[appid] = stats_save
-
-        except KeyError:
-            if verbose:
-                print("\nAppID:" + appid + "\tWilson score:" + str(wilson_score) + "\tName:" + name)
-            continue
+    except KeyError:
+        if verbose:
+            print("\nAppID:" + appid + "\tWilson score:" + str(wilson_score) + "\tName:" + name)
+        continue
 
 # First line of the text file containing the output dictionary
 leading_comment = "# Dictionary with key=appid and value=list of name, Wilson score, #owners, #players, median playtime, average playtime, boolean whether to include the game in the ranking"
