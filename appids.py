930cdd5e468f7e3b9681ebb4baabec1eb4248d48
Wok <woctezuma@users.noreply.github.com>
2017-11-29 00:53:30 +0100

    Documentation of games used as references for hidden gems

--- appids.py
+++ appids.py
@@ -1,18 +1,19 @@
 # Objective: list appID of examples of "hidden gems"
 
 # This is the appID of the game called "Contradiction".
-appidContradiction = "373390"
+appidContradiction = "373390"                           # Contradiction - Spot The Liar!
 
 # This is a set including appID of games which will serve as references of "hidden gems".
 # Reference: http://www.neogaf.com/forum/showpost.php?p=241232835&postcount=5886
-appid_hidden_gems_reference_set = {appidContradiction,
-                               "320090",
-                               "363980",
-                               "561740",
-                               "333300",
-                               "329970",
-                               "323220",
-                               "534290",
-                               "440880",
-                               "402040",
-                               "233980"}
+appid_hidden_gems_reference_set = {appidContradiction,  # Contradiction - Spot The Liar!
+                                   "320090",            # This Starry Midnight We Make
+                                   "363980",            # Forget Me Not: My Organic Garden
+                                   "561740",            # MidBoss
+                                   "333300",            # ADOM (Ancient Domains Of Mystery)
+                                   "329970",            # KeeperRL
+                                   "323220",            # Vagante
+                                   "534290",            # Cursed Castilla (Maldita Castilla EX)
+                                   "440880",            # The Count Lucanor
+                                   "402040",            # The Guest
+                                   "233980"             # UnEpic
+}
